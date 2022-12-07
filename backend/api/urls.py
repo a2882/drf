ff8340c7a733a36d7a44ffa1e94025ffa02e4ed7
Abhishek <abhinavc222444c@gmail.com>
2022-12-07 16:24:42 +0530

    *cheack*

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 # from .views import api_home
 
 
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', views.api_home), # localhost:8000/api/
     # path('products/', include('products.urls'))
 ]
