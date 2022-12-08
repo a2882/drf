e8308f8ed06ca1cdc9fd07cce7e17bb9f68ba61d
Abhishek <abhinavc222444c@gmail.com>
2022-12-08 22:03:50 +0530

    *cheack*

--- py_client/list.py
+++ py_client/list.py
@@ -2,12 +2,17 @@ import requests
 from getpass import getpass
 
 endpoint = "http://localhost:8000/api/auth/"
-password = getpass()
+username = input("what is your username: \n")
+password = getpass("enter your password: \n")
 
-auth_response = requests.post(endpoint, json={"username":"2882","password":password})
+auth_response = requests.post(endpoint, json={"username":username,"password":password})
 print(auth_response.json())
 
-
-endpoint = "http://localhost:8000/api/products/"
-get_response = requests.get(endpoint)
-print(get_response.json())
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f"Token {token}"
+        }
+    endpoint = "http://localhost:8000/api/products/"
+    get_response = requests.get(endpoint,headers=headers)
+    print(get_response.json())
