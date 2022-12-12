efcd3a458a67211f34c32b42da0f417a03c89aae
Abhishek <abhinavc222444c@gmail.com>
2022-12-12 13:49:59 +0530

    *cheack*

--- py_client/create.py
+++ py_client/create.py
@@ -3,7 +3,7 @@ import requests
 endpoint = "http://localhost:8000/api/products/" 
 
 data = {
-    "title": "This field is done",
+    "title": "This field is done 2.0",
     "price": 32.99
 }
 get_response = requests.post(endpoint, json=data) 
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,18 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/products/1/" 
+endpoint = "http://localhost:8000/api/auth/"
+username = input("what is your username: \n")
+password = getpass("enter your password: \n")
 
-get_response = requests.get(endpoint) 
+auth_response = requests.post(endpoint, json={"username":username,"password":password})
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f"Token {token}"
+        }
+endpoint = "http://localhost:8000/api/products/11/" 
+get_response = requests.get(endpoint,headers=headers) 
 print(get_response.json())
